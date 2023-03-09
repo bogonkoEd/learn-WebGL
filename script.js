69b8c6f69b908a35b18bd62b3bd623d050d57668
eddy.bogonko@strathmore.edu <eddy.bogonko@strathmore.edu>
2023-03-09 19:34:49 +0300

    First render blue cube

---
+++ script.js
@@ -0,0 +1,30 @@
+//SCENE
+const scene = new THREE.Scene();
+
+//Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+//SIZES
+const sizes = {
+  width: 1080,
+  height: 720,
+};
+
+//CAMERA
+const camera = new THREE.PerspectiveCamera(65, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+//RENDERER
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
