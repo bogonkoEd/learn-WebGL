642b2e9c217800964c21bf8d07a66f2b14de1e43
eddy.bogonko@strathmore.edu <eddy.bogonko@strathmore.edu>
2023-03-10 04:43:52 +0300

    Cursor-Camera Positioning and Controls

--- src/script.js
+++ src/script.js
@@ -1,5 +1,17 @@
 import "./style.css";
 import * as THREE from "three";
+import gsap from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+//CURSOR
+const cursor = {
+  x: 0,
+  y: 0,
+};
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = -(event.clientY / sizes.height - 0.5);
+  console.log(cursor.y);
+});
 
 //SCENE
 const scene = new THREE.Scene();
@@ -42,8 +54,27 @@ const sizes = {
 };
 
 //CAMERA
-const camera = new THREE.PerspectiveCamera(90, sizes.width / sizes.height);
-camera.position.set(-1, -1, 3);
+
+const camera = new THREE.PerspectiveCamera(
+  90, //FOV
+  sizes.width / sizes.height, //Aspect Ratio
+  0.1, //Near
+  30 //Far
+);
+
+//Orthographic Camera
+/*const aspectRatio = sizes.width / sizes.height;
+const camera = new THREE.OrthographicCamera(
+  -1 * aspectRatio, //Left
+  1 * aspectRatio, //Right
+  1, //Top
+  -1, //Bottom
+  0.1, //Near
+  30 //Far
+);*/
+
+camera.position.set(0, 0, 2);
+
 //LOOK AT FUNCTION
 
 scene.add(camera);
@@ -56,20 +87,34 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
-//ANIMATION
-const clock = new THREE.Clock();
+//CONTROLS
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
+//ANIMATION
+// gsap.to(group.position, { duration: 1, delay: 1, x: 2 });
+// gsap.to(group.position, { duration: 1, delay: 2, x: 0 });
+
+const clock = new THREE.Clock();
 const loop = () => {
   //TIMING
   const deltaTime = clock.getElapsedTime();
 
-  console.log(deltaTime);
   //New Frame
   window.requestAnimationFrame(loop);
+  //Update Camera Position
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 6;
+
+  //Update Camera Controls
+  controls.update();
+
+  camera.lookAt(group.position);
   //Update Objects Position
   group.rotation.y = deltaTime;
-  group.position.y = Math.sin(deltaTime);
-  group.position.z = Math.cos(deltaTime);
+  // group.position.y = Math.sin(deltaTime);
+  // group.position.z = Math.cos(deltaTime);
 
   camera.lookAt(group.position);
   //New Object Render
