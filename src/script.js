126799d3c11c973850bab2094e2e16f13296af4d
eddy.bogonko@strathmore.edu <eddy.bogonko@strathmore.edu>
2023-03-15 18:00:25 +0300

    Texture And Material

--- src/script.js
+++ src/script.js
@@ -5,6 +5,36 @@ import * as dat from "dat.gui";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { MeshBasicMaterial } from "three";
 
+//Textures
+const loading = new THREE.LoadingManager();
+loading.onStart = () => {
+  console.log("loading started");
+};
+loading.onLoad = () => {
+  console.log("loading finished");
+};
+
+const textureLoader = new THREE.TextureLoader(loading);
+const alphaTexture = textureLoader.load("/textures/door/alpha.jpg");
+const ambientTexture = textureLoader.load(
+  "/textures/door/ambientOcclusion.jpg"
+);
+const heightTexture = textureLoader.load("/textures/door/height.jpg");
+const normalTexture = textureLoader.load("/textures/door/normal.jpg");
+const metalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
+const roughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
+const colorTexture = textureLoader.load("/textures/minecraft.png");
+const matcapTexture = textureLoader.load("/textures/matcaps/7.png");
+const gradientTexture = textureLoader.load("/textures/gradients/3.jpg");
+
+colorTexture.rotation = Math.PI * 0.25;
+colorTexture.center.x = 0.5;
+colorTexture.center.y = 0.5;
+
+// Using NearestFilter will make the texture look pixelated and is better for performance than LinearFilter
+colorTexture.minFilter = THREE.NearestFilter;
+colorTexture.magFilter = THREE.NearestFilter;
+
 //GUI
 const gui = new dat.GUI();
 gui.hide();
@@ -25,7 +55,6 @@ const cursor = {
 window.addEventListener("mousemove", (event) => {
   cursor.x = event.clientX / sizes.width - 0.5;
   cursor.y = -(event.clientY / sizes.height - 0.5);
-  console.log(cursor.y);
 });
 
 //SCENE
@@ -45,32 +74,51 @@ const positionsArray = new Float32Array(count * 3 * 3);
 for (let i = 0; i < count * 3 * 3; i++) {
   positionsArray[i] = (Math.random() - 0.5) * 4;
 }
+//NEW BUFFER ATTRIBUTE - MeshBasicMaterial
 const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
 geometry.setAttribute("position", positionsAttribute);
 const material = new THREE.MeshBasicMaterial({
-  color: 0x0000ff,
-  wireframe: true,
+  color: 0x0000ff, // material.color.set(parameters.color);
+  map: colorTexture, // material.map = colorTexture;
+  wireframe: false, // material.wireframe = false;
+  opacity: 0.5, // material.opacity = 0.5;
+  transparent: true, // material.transparent = true;
+  // alphaMap: alphaTexture, // material.alphaMap = alphaTexture;
+  // side: THREE.DoubleSide, // material.side = THREE.DoubleSide;
+});
+
+//NEW BUFFER ATTRIBUTE - MeshNormalMaterial - This is the default material for lighting fx
+const material1 = new THREE.MeshNormalMaterial({
+  color: 0x0000ff, // material.color.set(parameters.color);
+  map: colorTexture, // material.map = colorTexture;
+  wireframe: false, // material.wireframe = false;
+  opacity: 0.5, // material.opacity = 0.5;
+  transparent: true, // material.transparent = true;
+  // alphaMap: alphaTexture, // material.alphaMap = alphaTexture;
+  // side: THREE.DoubleSide, // material.side = THREE.DoubleSide;
 });
 
 // const mesh = new THREE.Mesh(geometry, material);
 // scene.add(mesh);
 
 const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: parameters.color })
+  new THREE.SphereBufferGeometry(1, 32, 32),
+  material1
 );
+cube1.position.x = -3;
 group.add(cube1);
 
 const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: parameters.color, wireframe: true })
+  new THREE.PlaneBufferGeometry(1, 1, 2, 2),
+  material1
 );
-cube2.position.x = -3;
+
+cube2.position.x = 0;
 group.add(cube2);
 
 const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: parameters.color })
+  new THREE.TorusBufferGeometry(0.3, 0.25, 16, 32),
+  material1
 );
 cube3.position.x = 3;
 group.add(cube3);
@@ -129,24 +177,24 @@ window.addEventListener("dblclick", () => {
 //CAMERA
 
 const camera = new THREE.PerspectiveCamera(
-  90, //FOV
+  75, //FOV
   sizes.width / sizes.height, //Aspect Ratio
-  0.1, //Near
-  30 //Far
+  0.5, //Near
+  75 //Far
 );
 
 //Orthographic Camera
-/*const aspectRatio = sizes.width / sizes.height;
-const camera = new THREE.OrthographicCamera(
-  -1 * aspectRatio, //Left
-  1 * aspectRatio, //Right
-  1, //Top
-  -1, //Bottom
-  0.1, //Near
-  30 //Far
-);*/
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio, //Left
+//   1 * aspectRatio, //Right
+//   1, //Top
+//   -1, //Bottom
+//   0.1, //Near
+//   30 //Far
+// );
 
-camera.position.set(0, 0, 2);
+camera.position.set(0, 0, 3);
 
 //LOOK AT FUNCTION
 
